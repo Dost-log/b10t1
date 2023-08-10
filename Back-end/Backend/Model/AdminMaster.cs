9e8f6b6c99448f6b8da01c950b6abb1d628def3e
Sampada Joshi <joshisampada318@gmail.com>
2023-08-10 15:57:58 +0530

    context update

---
+++ Back-end/Backend/Model/AdminMaster.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Backend.Model
+{
+    public class AdminMaster
+    {
+            [Key]
+            [Column(TypeName = "varchar")]
+            [StringLength(6)]
+            public string EmployeeId { get; set; }
+
+            [Required]
+            [Column(TypeName = "varchar")]
+            [StringLength(20)]
+            public string name { get; set; }
+
+            [Column(TypeName = "varchar")]
+            [StringLength(25)]
+            public string designation { get; set; }
+
+            [Column(TypeName = "char")]
+            public char gender { get; set; }
+
+            [Column(TypeName = "varchar")]
+            [StringLength(25)]
+            public string department { get; set; }
+
+            [Column(TypeName = "DateOnly")]
+            public DateOnly DOB { get; set; }
+
+            [Column(TypeName = "DateOnly")]
+            public DateOnly DOJ { get; set; }
+        }
+    }
+
--- Back-end/Backend/Model/LoanDBContext.cs
+++ Back-end/Backend/Model/LoanDBContext.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Backend.Model;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Entities
 {
     public class LoanDBContext : DbContext
     {
 
-        public DbSet<ItemMaster> ItemMasters{get; set;}
+        public DbSet<ItemMaster> ItemMasters { get; set; }
+        public DbSet<AdminMaster> AdminMasters { get; set; }
+        public DbSet<EmployeeMaster> EmployeeMasters { get; set; }
+        public DbSet<LoanCardMaster> LoanCardMasters { get; set; }
+        public DbSet<EmployeeIssue> EmployeeIssues { get; set; }
+        public DbSet<EmployeeCard> EmployeeCards { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
